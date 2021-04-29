365dcb1d3127238bdcaa0064d5aef728f7f4b268
Matthew Hotchkiss <hotchkma@flip1.engr.oregonstate.edu>
2021-04-29 16:39:08 -0700

    basic functionality

--- calc.py
+++ calc.py
@@ -1,1 +1,18 @@
 #initialize calc.py
+
+def add(x,y):
+	print(x+y)
+
+def subtract(x,y):
+	print(x-y)
+
+def multiply(x,y):
+	print(x*y)
+
+def divide(x,y):
+	print(x/y)
+
+add(16,4)
+subtract(16,4)
+multiply(16,4)
+divide(16,4)
